0c1a0eece9ff4a3f751570aa699dbb955f352df0
ScSofts <scsofts@qq.com>
2020-02-06 16:07:47 +0800

    Create startup point

---
+++ plog.php
@@ -0,0 +1,42 @@
+<?php
+	$version="1.0.0";//The Version Code
+	
+	//Check to make sure it's running in the CLI mode
+	if(!isset($argc)||$argc<2){
+		
+		echo("Wrong useage!\n");
+		echo("Plog can`t run in the browser!\n");
+		echo("Use php plog.php -h or php plog.php --help for help\n");
+		echo("Plog V{$version} By Sc\n");
+		die("Github https://github.com/ScSofts/Plog.git\n");
+		exit(-1);
+	}
+
+	//Analyze the parameters
+	switch($argv[1]){
+		case '-h':
+		case '--help':
+			echo("Plog V{$version} By Sc\n");
+			echo("Github https://github.com/ScSofts/Plog.git\n");
+			echo("Usage php plog.php <Command> [Options]\n");
+			echo("Commands: \n");
+			echo(" build      => Generate or update static files\n");
+			echo(" preview    => Local preview in the browser\n");
+			echo(" create     => Create a project\n");
+			echo(" publish    => Publish in the github or other platforms\n");
+			echo("Options: \n");
+			echo(" For preview: \n");
+			echo("  -p <Port>  => The server port\n");
+			echo("  -a         => Auto open browser(require Windows)\n");
+			echo(" For create: \n");
+			echo("  -m         => Create a template project\n");
+			echo(" For build: \n");
+			echo("  -c         => Compress for less size\n");
+			break;
+		default:
+			echo("Unknown command {$argv[1]}!\n");
+			echo("Use php plog.php -h or php plog.php --help for help\n");
+			echo("Plog V{$version} By Sc\n");
+			die("Github https://github.com/ScSofts/Plog.git\n");
+	}
+?>
